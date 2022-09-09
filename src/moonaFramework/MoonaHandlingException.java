c317533615186dfe84f2baa22e7ca4a47381ba94
= <pelleale261@gmail.com>
2022-09-09 19:56:58 +0200

    [58] added some JavaDocs documentation.
    The MoonaHandlingException class was fully commented.

--- src/moonaFramework/MoonaHandlingException.java
+++ src/moonaFramework/MoonaHandlingException.java
@@ -1,13 +1,30 @@
 package moonaFramework;
 
+/**
+ * The MoonaHandlingException was created with the intent of warning the user of some problem happening in 
+ * relation to the Moona Class. When it comes to some incorrect argument or to some wrong usage of a Moona 
+ * method, this exception gets thrown to warn of those kind of issues. There is no such case in which a 
+ * MoonaHandlingException can be thrown by some other class than the Moona Class itself.
+ * 
+ * @author MasterZEr0
+ */
 public class MoonaHandlingException extends RuntimeException implements Serial {
 
+	/**
+	 * this long value stores an unique ID to identify this exception. This number is unique for each 
+	 * exception class, in this case, it assumes the value of -1.
+	 */
 	private static final long serialVersionUID = -1;
 	
+	/**
+	 * ALL the exceptions return their #serialVersionUID as ID.
+	 */
+	@Override
 	public final long id() {
 		return serialVersionUID;
 	}
 	
+	@Override
 	public final int nature() {
 		return Moona.EXCEPTION;
 	}
