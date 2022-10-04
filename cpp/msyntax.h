cb36456337391e0b92fc275b13f30916a87fb6de
anOsuPlayer <pelleale261@gmail.com>
2022-10-04 10:25:53 +0200

    [111] added first smol C++ Header.
    "msyntax.h" header was added: it adds minor syntax features.

---
+++ cpp/msyntax.h
@@ -0,0 +1,17 @@
+#include <iostream>
+
+#define foreach(FROM, TO) for (ssize_t i = FROM; i < TO; i++)
+#define repeat(TIMES) for (ssize_t i = 0; i < TIMES; i++)
+
+#define elif else if
+#define in :
+
+#define ptr *
+#define at &
+
+#define public public:
+#define protected protected:
+#define private private:
+
+#define abstract virtual
+#define constant static const
