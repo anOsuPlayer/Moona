d82695b44ad60a4ff921fe4b08578f251f0669b5
= <pelleale261@gmail.com>
2022-10-21 23:09:33 +0200

    [189] AutoEvent now implements Dynamic, too.

--- java/src/moonaFramework/event/AutoEvent.java
+++ java/src/moonaFramework/event/AutoEvent.java
@@ -1,9 +1,10 @@
 package moonaFramework.event;
 
 import moonaFramework.Moona;
+import moonaFramework.essentials.Dynamic;
 import moonaFramework.process.Task;
 
-public abstract class AutoEvent extends Task implements Event {
+public abstract class AutoEvent extends Task implements Event, Dynamic {
 	
 	@Override
 	public abstract void trigger();
