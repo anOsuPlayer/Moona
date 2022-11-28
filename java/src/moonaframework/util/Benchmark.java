3741926d089bbe4cfda14456cd38d818cf35f866
anOsuPlayer <pelleale261@gmail.com>
2022-11-28 08:44:41 +0100

    [294] added minor methods to Benchmark Class.

--- java/src/moonaframework/util/Benchmark.java
+++ java/src/moonaframework/util/Benchmark.java
@@ -9,6 +9,9 @@ public class Benchmark {
 		s.code();
 		return System.nanoTime()-beg;
 	}
+	public static final void showTime(Snippet s) {
+		System.out.println(Benchmark.time(s));
+	}
 	
 	public static final double stress(Snippet s, int iterations) {
 		double total = 0;
@@ -16,6 +19,9 @@ public class Benchmark {
 			total += time(s);
 		}
 		return total / iterations;
+	}
+	public static final void showStress(Snippet s, int iterations) {
+		System.out.println(Benchmark.stress(s, iterations));
 	}
 	
 	public static final void sleep(long mills) {
