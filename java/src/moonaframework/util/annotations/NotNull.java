968ed4f19b7ae2a1c51b74c50799f99eb1e79f31
anOsuPlayer <pelleale261@gmail.com>
2022-12-02 13:26:06 +0100

    [312] added NotNull and Nullable Annotations.
    They are required to mark those elements which explicitly can or cannot be null.

---
+++ java/src/moonaframework/util/annotations/NotNull.java
@@ -0,0 +1,18 @@
+package moonaframework.util.annotations;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.LOCAL_VARIABLE;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.ElementType.RECORD_COMPONENT;
+import static java.lang.annotation.ElementType.TYPE_USE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Retention(RUNTIME)
+@Target({ FIELD, PARAMETER, LOCAL_VARIABLE, ANNOTATION_TYPE, TYPE_USE, RECORD_COMPONENT })
+public @interface NotNull {
+
+}
