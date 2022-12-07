d5db413798680d73c030110ca0e68431501a9039
= <pelleale261@gmail.com>
2022-12-08 00:07:09 +0100

    [347] Nature Enum methods now detect AutoEvents.
    Methods such as Nature.isProcessLike(Natural n) will now detect AutoEvents too.

--- java/src/moonaframework/base/Nature.java
+++ java/src/moonaframework/base/Nature.java
@@ -21,11 +21,12 @@ public enum Nature {
 	REFLECTION;
 	
 	public static final boolean isProcessLike(Natural n) {
-		return n.nature().equals(PROCESS) || n.nature().equals(DAEMON) || n.nature().equals(WORM);
+		return n.nature().equals(PROCESS) || n.nature().equals(DAEMON) || n.nature().equals(WORM)
+				|| n.nature().equals(AUTOEVENT);
 	}
 	
 	public static final boolean isEventLike(Natural n) {
-		return n.nature().equals(EVENT) || n.nature().equals(MODALEVENT);
+		return n.nature().equals(EVENT) || n.nature().equals(MODALEVENT) || n.nature().equals(AUTOEVENT);
 	}
 	
 	public static final boolean isReflectionLike(Natural n) {
