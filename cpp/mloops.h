c4b1c95f8bcc2d0f4ceaa3c794e68739722fc0fa
= <pelleale261@gmail.com>
2022-12-11 16:47:38 +0100

    [367] removed descriptions from mloops.h Header.

--- cpp/mloops.h
+++ cpp/mloops.h
@@ -1,28 +1,10 @@
-/**
- * @file mloops.h
- * @author Master_ZEr0
- * @brief This header contains some special loops that might fit well in some scenarios.
- */
-
 #pragma once
 #ifndef mloops
 
     #define mloops
 
-    /**
-     * @brief A loop which, for a given variable "VAR", starting from 0, repeats until the variable reaches the
-     * "times" parameter.
-     */
     #define repeat(VAR, times) for (ssize_t VAR = 0; VAR < times; VAR++)
-    /**
-     * @brief It's a loop which, for a given variable "VAR", starting from the value of the variable "from",
-     * iterates until reaching the value of the third parameter "to".
-     */
     #define range(VAR, from, to) for (ssize_t VAR = from; VAR < to; VAR++)
-    /**
-     * @brief Basically, the opposite of a while loop. Executes the given block of code until it's proven
-     * false (it wouldn't be called "until loop" otherwise, would it?). 
-     */
     #define until(condition) while(!(condition))
 
 #endif
