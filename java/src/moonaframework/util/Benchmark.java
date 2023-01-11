ef93ba9afd78121196a92eec89ac9ba5a5a91707
= <pelleale261@gmail.com>
2023-01-11 21:23:33 +0100

    [538] added RecordContent Flare.
    A Flare containing all the RecordComponents of a Type Reference targeting a record.

--- java/src/moonaframework/util/Benchmark.java
+++ java/src/moonaframework/util/Benchmark.java
@@ -1,28 +1,19 @@
 package moonaframework.util;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import moonaframework.util.functional.Snippet;
 
 public final class Benchmark {
 	
-	private static float benchmarkUnit;
-	
-	private static float computeBenchmarkUnit() {
-		float bu = 0;
-		for (int i = 0; i < 100; i++) {
-			long now = System.nanoTime();
-			for (int e = 0; e < 10000; e++);
-			bu += System.nanoTime()-now;
-		}
-		return bu/100.0f;
-	}
-	
-	public static float time(Snippet s) {
+	public static long time(Snippet s) {
 		long beg = System.nanoTime();
 		s.run();
-		return ((System.nanoTime()-beg)*1.0f)/(benchmarkUnit*1.0f);
+		return System.nanoTime()-beg;
 	}
 	public static void showTime(Snippet s) {
-		System.out.println(Benchmark.time(s) + " bu");
+		System.out.println(Benchmark.time(s) + " ns");
 	}
 	
 	public static void sleep(long mills) {
@@ -31,10 +22,6 @@ public final class Benchmark {
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-	}
-	
-	static {
-		benchmarkUnit = computeBenchmarkUnit();
 	}
 	
 	private Benchmark() {
--- java/src/moonaframework/util/reflection/flare/RecordContent.java
+++ java/src/moonaframework/util/reflection/flare/RecordContent.java
@@ -1,6 +1,14 @@
 package moonaframework.util.reflection.flare;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import moonaframework.base.MoonaHandlingException;
 import moonaframework.util.exception.NullArgumentException;
+import moonaframework.util.exception.ReflectionNotFoundException;
+import moonaframework.util.exception.UndefinedReflectionException;
+import moonaframework.util.reflection.ExistingRecordComponent;
+import moonaframework.util.reflection.Parameter;
 import moonaframework.util.reflection.RecordComponent;
 import moonaframework.util.reflection.Type;
 
@@ -10,6 +18,66 @@ public final class RecordContent extends Flare<RecordComponent> {
 	
 	public @Override Type getTarget() {
 		return this.target;
+	}
+	
+	public int componentCount() {
+		return super.value.size();
+	}
+	public List<RecordComponent> getRecordComponents() throws MoonaHandlingException {
+		if (!super.hasGenerated) {
+			try {
+				reflect();
+			}
+			catch (UndefinedReflectionException ure) {
+				throw new MoonaHandlingException("Unable to operate with undefined Reflections.", ure);
+			}
+		}
+		
+		return super.value;
+	}
+	
+	public RecordComponent getRecordComponent(int index) throws IllegalArgumentException, MoonaHandlingException {
+		if (!super.hasGenerated) {
+			try {
+				reflect();
+			}
+			catch (UndefinedReflectionException ure) {
+				throw new MoonaHandlingException("Unable to operate with undefined Reflections.", ure);
+			}
+		}
+		
+		if (index < 0) {
+			throw new IllegalArgumentException("Negative indexes are not allowed.");
+		}
+		if (index >= super.value.size()) {
+			throw new IllegalArgumentException("There are only " + (super.value.size()-1) + " RecordComponent "
+					+ "References, index " + index + " is out of range.");
+		}
+		
+		return super.value.get(index);
+	}
+	public RecordComponent getRecordComponent(String name) throws NullArgumentException, MoonaHandlingException, ReflectionNotFoundException {
+		if (!super.hasGenerated) {
+			try {
+				reflect();
+			}
+			catch (UndefinedReflectionException ure) {
+				throw new MoonaHandlingException("Unable to operate with undefined Reflections.", ure);
+			}
+		}
+		
+		if (name == null) {
+			throw new NullArgumentException("The record component's name cannot be null.");
+		}
+		
+		for (RecordComponent rc : super.value) {
+			if (rc.getName().equals(name)) {
+				return rc;
+			}
+		}
+		
+		throw new ReflectionNotFoundException("There is no record component named " + name + " in this"
+				+ " RecordContent.");
 	}
 	
 	public @Override boolean equals(Object o) {
@@ -22,8 +90,18 @@ public final class RecordContent extends Flare<RecordComponent> {
 		return (!super.hasGenerated) ? "Non-generated Flare." : "RecordContent of " + target;
 	}
 	
-	public @Override void reflect() {
+	public @Override void reflect() throws UndefinedReflectionException {
+		@SuppressWarnings("unchecked") Class<? extends Record> clazz = (Class<? extends Record>) target.evaluate();
 		
+		strictContext.enable();
+		
+		for (java.lang.reflect.RecordComponent rc : clazz.getRecordComponents()) {
+			super.value.add(new ExistingRecordComponent(clazz, rc.getName(), rc));
+		}
+		
+		strictContext.disable();
+		
+		super.reflect();
 	}
 
 	public RecordContent(Type target) throws IllegalArgumentException, NullArgumentException {
