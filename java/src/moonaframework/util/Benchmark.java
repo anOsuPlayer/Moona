88f1eb4d6ed875badd25786b8ef5b4ba1001411b
anOsuPlayer <pelleale261@gmail.com>
2023-01-13 13:22:57 +0100

    [548] improved Benchmark class.
    Added proper exception handling and reintroduced stress() and showStress() methods (they're used to get the average time it takes a Snippet to be executed N times).

--- java/src/moonaframework/util/Benchmark.java
+++ java/src/moonaframework/util/Benchmark.java
@@ -1,16 +1,39 @@
 package moonaframework.util;
 
+import moonaframework.util.exception.NullArgumentException;
 import moonaframework.util.functional.Snippet;
 
 public final class Benchmark {
 	
-	public static long time(Snippet s) {
+	public static long time(Snippet s) throws NullArgumentException {
+		if (s == null) {
+			throw new NullArgumentException("Cannot time a null Snippet.");	
+		}
+		
 		long beg = System.nanoTime();
 		s.run();
 		return System.nanoTime()-beg;
 	}
 	public static void showTime(Snippet s) {
 		System.out.println(Benchmark.time(s) + " ns");
+	}
+	
+	public static double stress(int cycles, Snippet s) throws IllegalArgumentException, NullArgumentException {
+		if (s == null) {
+			throw new NullArgumentException("Cannot stress a null Snippet.");
+		}
+		if (cycles <= 0) {
+			throw new IllegalArgumentException("Must iterate at least one time.");
+		}
+		
+		double avg = 0.0;
+		for (int i = 0; i < cycles; i++) {
+			avg += time(s);
+		}
+		return avg / cycles;
+	}
+	public static void showStress(int cycles, Snippet s) {
+		System.out.println("avg of: " + stress(cycles, s) + " ns");
 	}
 	
 	public static void sleep(long mills) {
