7e47ec564d6c244dd812a5eacdbf8d7d409b0582
anOsuPlayer <pelleale261@gmail.com>
2023-02-02 09:39:49 +0100

    [600] removed all the Types and left only the original one.

--- cpp/base/base.hpp
+++ cpp/base/base.hpp
@@ -3,11 +3,6 @@
 #ifndef MOONA_BASE
     #define MOONA_BASE
 
-    #include "type/constptrtype.hpp"
-    #include "type/constreftype.hpp"
-    #include "type/consttype.hpp"
-    #include "type/ptrtype.hpp"
-    #include "type/reftype.hpp"
-    #include "type/type.hpp"
+    #include "type.hpp"
 
 #endif
