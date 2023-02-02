e9e7b5f247e20b37af36a60ff059120f482ddda0
anOsuPlayer <pelleale261@gmail.com>
2023-02-02 08:11:46 +0100

    [598] removed C++ files (once again).

--- cpp/base/base.hpp
+++ cpp/base/base.hpp
@@ -3,10 +3,6 @@
 #ifndef MOONA_BASE
     #define MOONA_BASE
 
-    #include "moonaclass.hpp"
-    #include "type.hpp"
-    #include "basicconcepts.hpp"
-    #include "object.hpp"
-    #include "entity.hpp"
+    
 
 #endif
