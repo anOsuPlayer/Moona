7cef909e16367c3aba8e4e59e5af40f95dc7111f
= <pelleale261@gmail.com>
2023-02-03 18:25:30 +0100

    [605] added moonaconcepts.hpp header.
    It will contain all the general-purpose C++ concepts for Moona.

---
+++ cpp/base/moonaconcepts.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#ifndef MOONA_CONCEPTS
+    #define MOONA_CONCEPTS
+
+    #include <concepts>
+
+    #include "object.hpp"
+    #include "entity.hpp"
+
+    namespace moona {
+
+        template <typename O, typename... super> concept MoonaObject = requires {
+            std::is_base_of<Object<O, super...>, O>();
+        };
+
+        template <typename E, typename... super> concept MoonaEntity = requires {
+            std::is_base_of<Entity<E, super...>, E>();
+        };
+        template <typename E, typename... super> concept PureEntity = requires {
+            MoonaEntity<E, super...> && !MoonaObject<E, super...>;
+        };
+    }
+
+#endif
