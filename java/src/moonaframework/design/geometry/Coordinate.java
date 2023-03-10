2cee51db2f66be0fc6c3d346d076827771214145
anOsuPlayer <pelleale261@gmail.com>
2023-03-10 08:54:23 +0100

    [714] removed Coordinate and Size enums.
    With this new system they'd be pretty much useless. However, since I know myself, it's very likely of me to add them back in a few days.

--- java/src/moonaframework/design/geometry/Coordinate.java
+++
@@ -1,6 +0,0 @@
-package moonaframework.design.geometry;
-
-public enum Coordinate {
-
-	X, Y, Z;
-}
--- java/src/moonaframework/design/geometry/Size.java
+++
@@ -1,6 +0,0 @@
-package moonaframework.design.geometry;
-
-public enum Size {
-
-	WIDTH, HEIGHT, DEPTH;
-}
