24fd39d782bdc0f1d6f78673ff60b6177d75a0ee
anOsuPlayer <pelleale261@gmail.com>
2023-03-14 13:27:40 +0100

    [723] added exception declarations, .move() and .resize() methods.
    The .move() and the .resize() method both allow a quick way to edit multiple coordinates/dimensions of a GeometricElement at once.

--- java/src/moonaframework/design/geometry/MovableType.java
+++ java/src/moonaframework/design/geometry/MovableType.java
@@ -2,17 +2,24 @@ package moonaframework.design.geometry;
 
 public interface MovableType<T extends Number> extends PositionalType<T> {
 
-	void setX(T x);
+	void setX(T x) throws NullPointerException;
 	
-	void moveX(T dx);
+	void moveX(T dx) throws NullPointerException;
 	
-	void setY(T y);
+	void setY(T y) throws NullPointerException, LowerDimensionalOrderException;
 	
-	void moveY(T dy);
+	void moveY(T dy) throws NullPointerException, LowerDimensionalOrderException;
 	
-	void setZ(T z);
+	void setZ(T z) throws NullPointerException, LowerDimensionalOrderException;
 	
-	void moveZ(T dz);
+	void moveZ(T dz) throws NullPointerException, LowerDimensionalOrderException;
+	
+	default void move(T dx, T dy, T dz) throws NullPointerException, LowerDimensionalOrderException {
+		moveX(dx); moveY(dy); moveZ(dz);
+	}
+	default void move(T dx, T dy) throws NullPointerException, LowerDimensionalOrderException {
+		moveX(dx); moveY(dy);
+	}
 	
 	public static interface IntegralMovable extends MovableType<Integer>, IntegralPositional {
 		
@@ -21,35 +28,48 @@ public interface MovableType<T extends Number> extends PositionalType<T> {
 		default void moveX(int dx) {
 			setX(getX() + dx);
 		}
-		default @Override void setX(Integer x) {
+		default @Override void setX(Integer x) throws NullPointerException {
 			setX(x.intValue());
 		}
-		default @Override void moveX(Integer dx) {
+		default @Override void moveX(Integer dx) throws NullPointerException {
 			moveX(dx.intValue());
 		}
 		
-		void setY(int y);
+		void setY(int y) throws LowerDimensionalOrderException;
 		
-		default void moveY(int dy) {
+		default void moveY(int dy) throws LowerDimensionalOrderException {
 			setY(getY() + dy);
 		}
-		default @Override void setY(Integer y) {
+		default @Override void setY(Integer y) throws NullPointerException, LowerDimensionalOrderException {
 			setY(y.intValue());
 		}
-		default @Override void moveY(Integer dy) {
+		default @Override void moveY(Integer dy) throws NullPointerException, LowerDimensionalOrderException {
 			moveY(dy.intValue());
 		}
 		
-		void setZ(int z);
+		void setZ(int z) throws LowerDimensionalOrderException;
 		
-		default void moveZ(int dz) {
+		default void moveZ(int dz) throws LowerDimensionalOrderException {
 			setZ(getZ() + dz);
 		}
-		default @Override void setZ(Integer z) {
+		default @Override void setZ(Integer z) throws NullPointerException, LowerDimensionalOrderException {
 			setZ(z.intValue());
 		}
-		default @Override void moveZ(Integer dz) {
+		default @Override void moveZ(Integer dz) throws NullPointerException, LowerDimensionalOrderException {
 			moveZ(dz.intValue());
+		}
+		
+		default @Override void move(Integer dx, Integer dy, Integer dz) throws NullPointerException, LowerDimensionalOrderException {
+			moveX(dx.intValue()); moveY(dy.intValue()); moveZ(dz.intValue());
+		}
+		default void move(int dx, int dy, int dz) throws LowerDimensionalOrderException {
+			moveX(dx); moveY(dy); moveZ(dz);
+		}
+		default @Override void move(Integer dx, Integer dy) throws NullPointerException, LowerDimensionalOrderException {
+			moveX(dx.intValue()); moveY(dy.intValue());
+		}
+		default void move(int dx, int dy) throws LowerDimensionalOrderException {
+			moveX(dx); moveY(dy);
 		}
 	}
 	
@@ -60,35 +80,48 @@ public interface MovableType<T extends Number> extends PositionalType<T> {
 		default void moveX(float dx) {
 			setX(getX() + dx);
 		}
-		default @Override void setX(Float x) {
+		default @Override void setX(Float x) throws NullPointerException {
 			setX(x.floatValue());
 		}
-		default @Override void moveX(Float dx) {
+		default @Override void moveX(Float dx) throws NullPointerException {
 			moveX(dx.floatValue());
 		}
 		
-		void setY(float y);
+		void setY(float y) throws LowerDimensionalOrderException;
 		
-		default void moveY(float dy) {
+		default void moveY(float dy) throws LowerDimensionalOrderException {
 			setY(getY() + dy);
 		}
-		default @Override void setY(Float y) {
+		default @Override void setY(Float y) throws NullPointerException, LowerDimensionalOrderException {
 			setY(y.floatValue());
 		}
-		default @Override void moveY(Float dy) {
+		default @Override void moveY(Float dy) throws NullPointerException, LowerDimensionalOrderException {
 			moveY(dy.floatValue());
 		}
 		
-		void setZ(float z);
+		void setZ(float z) throws LowerDimensionalOrderException;
 		
-		default void moveZ(float dz) {
+		default void moveZ(float dz) throws LowerDimensionalOrderException {
 			setZ(getZ() + dz);
 		}
-		default @Override void setZ(Float z) {
+		default @Override void setZ(Float z) throws NullPointerException, LowerDimensionalOrderException {
 			setZ(z.floatValue());
 		}
-		default @Override void moveZ(Float dz) {
+		default @Override void moveZ(Float dz) throws NullPointerException, LowerDimensionalOrderException {
 			moveZ(dz.floatValue());
+		}
+		
+		default @Override void move(Float dx, Float dy, Float dz) throws NullPointerException, LowerDimensionalOrderException {
+			moveX(dx.floatValue()); moveY(dy.floatValue()); moveZ(dz.floatValue());
+		}
+		default void move(float dx, float dy, float dz) throws LowerDimensionalOrderException {
+			moveX(dx); moveY(dy); moveZ(dz);
+		}
+		default @Override void move(Float dx, Float dy) throws NullPointerException, LowerDimensionalOrderException {
+			moveX(dx.floatValue()); moveY(dy.floatValue());
+		}
+		default void move(float dx, float dy) throws LowerDimensionalOrderException {
+			moveX(dx); moveY(dy);
 		}
 	}
 	
@@ -99,35 +132,48 @@ public interface MovableType<T extends Number> extends PositionalType<T> {
 		default void moveX(double dx) {
 			setX(getX() + dx);
 		}
-		default @Override void setX(Double x) {
+		default @Override void setX(Double x) throws NullPointerException {
 			setX(x.doubleValue());
 		}
-		default @Override void moveX(Double dx) {
+		default @Override void moveX(Double dx) throws NullPointerException {
 			moveX(dx.doubleValue());
 		}
 		
-		void setY(double y);
+		void setY(double y) throws LowerDimensionalOrderException;
 		
-		default void moveY(double dy) {
+		default void moveY(double dy) throws LowerDimensionalOrderException {
 			setY(getY() + dy);
 		}
-		default @Override void setY(Double y) {
+		default @Override void setY(Double y) throws NullPointerException, LowerDimensionalOrderException {
 			setY(y.doubleValue());
 		}
-		default @Override void moveY(Double dy) {
+		default @Override void moveY(Double dy) throws NullPointerException, LowerDimensionalOrderException {
 			moveY(dy.doubleValue());
 		}
 		
-		void setZ(double z);
+		void setZ(double z) throws LowerDimensionalOrderException;
 		
-		default void moveZ(double dz) {
+		default void moveZ(double dz) throws LowerDimensionalOrderException {
 			setZ(getZ() + dz);
 		}
-		default @Override void setZ(Double z) {
+		default @Override void setZ(Double z) throws NullPointerException, LowerDimensionalOrderException {
 			setZ(z.doubleValue());
 		}
-		default @Override void moveZ(Double dz) {
+		default @Override void moveZ(Double dz) throws NullPointerException, LowerDimensionalOrderException {
 			moveZ(dz.doubleValue());
+		}
+		
+		default @Override void move(Double dx, Double dy, Double dz) throws NullPointerException, LowerDimensionalOrderException {
+			moveX(dx.doubleValue()); moveY(dy.doubleValue()); moveZ(dz.doubleValue());
+		}
+		default void move(double dx, double dy, double dz) throws LowerDimensionalOrderException {
+			moveX(dx); moveY(dy); moveZ(dz);
+		}
+		default @Override void move(Double dx, Double dy) throws NullPointerException, LowerDimensionalOrderException {
+			moveX(dx.doubleValue()); moveY(dy.doubleValue());
+		}
+		default void move(double dx, double dy) throws LowerDimensionalOrderException {
+			moveX(dx); moveY(dy);
 		}
 	}
 }
--- java/src/moonaframework/design/geometry/ResizableType.java
+++ java/src/moonaframework/design/geometry/ResizableType.java
@@ -2,17 +2,24 @@ package moonaframework.design.geometry;
 
 public interface ResizableType<T extends Number> extends DimensionalType<T> {
 
-	void setWidth(T width);
+	void setWidth(T width) throws NullPointerException;
 	
-	void resizeWidth(T dWidth);
+	void resizeWidth(T dWidth) throws NullPointerException;
 	
-	void setHeight(T height);
+	void setHeight(T height) throws NullPointerException, LowerDimensionalOrderException;
 	
-	void resizeHeight(T dHeight);
+	void resizeHeight(T dHeight) throws NullPointerException, LowerDimensionalOrderException;
 	
-	void setDepth(T depth);
+	void setDepth(T depth) throws NullPointerException, LowerDimensionalOrderException;
 	
-	void resizeDepth(T dDepth);
+	void resizeDepth(T dDepth) throws NullPointerException, LowerDimensionalOrderException;
+	
+	default void resize(T dWidth, T dHeight, T dDepth) throws NullPointerException, LowerDimensionalOrderException {
+		resizeWidth(dWidth); resizeHeight(dHeight); resizeDepth(dDepth);
+	}
+	default void resize(T dWdith, T dHeight) throws NullPointerException, LowerDimensionalOrderException {
+		resizeWidth(dWdith); resizeHeight(dHeight);
+	}
 	
 	public static interface IntegralResizable extends ResizableType<Integer>, IntegralDimensional {
 		
@@ -21,35 +28,48 @@ public interface ResizableType<T extends Number> extends DimensionalType<T> {
 		default void resizeWidth(int dWidth) {
 			setWidth(getWidth() + dWidth);
 		}
-		default @Override void setWidth(Integer width) {
+		default @Override void setWidth(Integer width) throws NullPointerException {
 			setWidth(width.intValue());
 		}
-		default @Override void resizeWidth(Integer dWidth) {
+		default @Override void resizeWidth(Integer dWidth) throws NullPointerException {
 			resizeWidth(dWidth.intValue());
 		}
 		
-		void setHeight(int height);
+		void setHeight(int height) throws LowerDimensionalOrderException;
 		
-		default void resizeHeight(int dHeight) {
+		default void resizeHeight(int dHeight) throws LowerDimensionalOrderException {
 			setHeight(getHeight() + dHeight);
 		}
-		default @Override void setHeight(Integer height) {
+		default @Override void setHeight(Integer height) throws NullPointerException, LowerDimensionalOrderException {
 			setHeight(height.intValue());
 		}
-		default @Override void resizeHeight(Integer dHeight) {
+		default @Override void resizeHeight(Integer dHeight) throws NullPointerException, LowerDimensionalOrderException {
 			resizeHeight(dHeight.intValue());
 		}
 		
-		void setDepth(int depth);
+		void setDepth(int depth) throws LowerDimensionalOrderException;
 		
-		default void resizeDepth(int dDepth) {
+		default void resizeDepth(int dDepth) throws LowerDimensionalOrderException {
 			setDepth(getDepth() + dDepth);
 		}
-		default @Override void setDepth(Integer depth) {
+		default @Override void setDepth(Integer depth) throws NullPointerException, LowerDimensionalOrderException {
 			setDepth(depth.intValue());
 		}
-		default @Override void resizeDepth(Integer dDepth) {
+		default @Override void resizeDepth(Integer dDepth) throws NullPointerException, LowerDimensionalOrderException {
 			resizeDepth(dDepth.intValue());
+		}
+		
+		default @Override void resize(Integer dWidth, Integer dHeight, Integer dDepth) throws NullPointerException, LowerDimensionalOrderException {
+			resizeWidth(dWidth.intValue()); resizeHeight(dHeight.intValue()); resizeDepth(dDepth.intValue());
+		}
+		default void resize(int dWidth, int dHeight, int dDepth) throws LowerDimensionalOrderException {
+			resizeWidth(dWidth); resizeHeight(dHeight); resizeDepth(dDepth);
+		}
+		default @Override void resize(Integer dWidth, Integer dHeight) throws NullPointerException, LowerDimensionalOrderException {
+			resizeWidth(dWidth.intValue()); resizeHeight(dHeight.intValue());
+		}
+		default void resize(int dWidth, int dHeight) throws LowerDimensionalOrderException {
+			resizeWidth(dWidth); resizeHeight(dHeight);
 		}
 	}
 	
@@ -60,35 +80,48 @@ public interface ResizableType<T extends Number> extends DimensionalType<T> {
 		default void resizeWidth(float dWidth) {
 			setWidth(getWidth() + dWidth);
 		}
-		default @Override void setWidth(Float width) {
+		default @Override void setWidth(Float width) throws NullPointerException {
 			setWidth(width.floatValue());
 		}
-		default @Override void resizeWidth(Float dWidth) {
+		default @Override void resizeWidth(Float dWidth) throws NullPointerException {
 			resizeWidth(dWidth.floatValue());
 		}
 		
-		void setHeight(float height);
+		void setHeight(float height) throws LowerDimensionalOrderException;
 		
-		default void resizeHeight(float dHeight) {
+		default void resizeHeight(float dHeight) throws LowerDimensionalOrderException {
 			setHeight(getHeight() + dHeight);
 		}
-		default @Override void setHeight(Float height) {
+		default @Override void setHeight(Float height) throws NullPointerException, LowerDimensionalOrderException {
 			setHeight(height.floatValue());
 		}
-		default @Override void resizeHeight(Float dHeight) {
+		default @Override void resizeHeight(Float dHeight) throws NullPointerException, LowerDimensionalOrderException {
 			resizeHeight(dHeight.floatValue());
 		}
 		
-		void setDepth(float depth);
+		void setDepth(float depth) throws LowerDimensionalOrderException;
 		
-		default void resizeDepth(float dDepth) {
+		default void resizeDepth(float dDepth) throws LowerDimensionalOrderException {
 			setDepth(getDepth() + dDepth);
 		}
-		default @Override void setDepth(Float depth) {
+		default @Override void setDepth(Float depth) throws NullPointerException, LowerDimensionalOrderException {
 			setDepth(depth.floatValue());
 		}
-		default @Override void resizeDepth(Float dDepth) {
+		default @Override void resizeDepth(Float dDepth) throws NullPointerException, LowerDimensionalOrderException {
 			resizeDepth(dDepth.floatValue());
+		}
+		
+		default @Override void resize(Float dWidth, Float dHeight, Float dDepth) throws NullPointerException, LowerDimensionalOrderException {
+			resizeWidth(dWidth.intValue()); resizeHeight(dHeight.intValue()); resizeDepth(dDepth.intValue());
+		}
+		default void resize(float dWidth, float dHeight, float dDepth) throws LowerDimensionalOrderException {
+			resizeWidth(dWidth); resizeHeight(dHeight); resizeDepth(dDepth);
+		}
+		default @Override void resize(Float dWidth, Float dHeight) throws NullPointerException, LowerDimensionalOrderException {
+			resizeWidth(dWidth.intValue()); resizeHeight(dHeight.intValue());
+		}
+		default void resize(float dWidth, float dHeight) throws LowerDimensionalOrderException {
+			resizeWidth(dWidth); resizeHeight(dHeight);
 		}
 	}
 	
@@ -99,35 +132,48 @@ public interface ResizableType<T extends Number> extends DimensionalType<T> {
 		default void resizeWidth(double dWidth) {
 			setWidth(getWidth() + dWidth);
 		}
-		default @Override void setWidth(Double width) {
+		default @Override void setWidth(Double width) throws NullPointerException {
 			setWidth(width.doubleValue());
 		}
-		default @Override void resizeWidth(Double dWidth) {
+		default @Override void resizeWidth(Double dWidth) throws NullPointerException {
 			resizeWidth(dWidth.doubleValue());
 		}
 		
-		void setHeight(double height);
+		void setHeight(double height) throws LowerDimensionalOrderException;
 		
-		default void resizeHeight(double dHeight) {
+		default void resizeHeight(double dHeight) throws LowerDimensionalOrderException {
 			setHeight(getHeight() + dHeight);
 		}
-		default @Override void setHeight(Double height) {
+		default @Override void setHeight(Double height) throws NullPointerException, LowerDimensionalOrderException {
 			setHeight(height.doubleValue());
 		}
-		default @Override void resizeHeight(Double dHeight) {
+		default @Override void resizeHeight(Double dHeight) throws NullPointerException, LowerDimensionalOrderException {
 			resizeHeight(dHeight.doubleValue());
 		}
 		
-		void setDepth(double depth);
+		void setDepth(double depth) throws LowerDimensionalOrderException;
 		
-		default void resizeDepth(double dDepth) {
+		default void resizeDepth(double dDepth) throws LowerDimensionalOrderException {
 			setDepth(getDepth() + dDepth);
 		}
-		default @Override void setDepth(Double depth) {
+		default @Override void setDepth(Double depth) throws NullPointerException, LowerDimensionalOrderException {
 			setDepth(depth.doubleValue());
 		}
-		default @Override void resizeDepth(Double dDepth) {
+		default @Override void resizeDepth(Double dDepth) throws NullPointerException, LowerDimensionalOrderException {
 			resizeDepth(dDepth.doubleValue());
+		}
+		
+		default @Override void resize(Double dWidth, Double dHeight, Double dDepth) throws NullPointerException, LowerDimensionalOrderException {
+			resizeWidth(dWidth.intValue()); resizeHeight(dHeight.intValue()); resizeDepth(dDepth.intValue());
+		}
+		default void resize(double dWidth, double dHeight, double dDepth) throws LowerDimensionalOrderException {
+			resizeWidth(dWidth); resizeHeight(dHeight); resizeDepth(dDepth);
+		}
+		default @Override void resize(Double dWidth, Double dHeight) throws NullPointerException, LowerDimensionalOrderException {
+			resizeWidth(dWidth.intValue()); resizeHeight(dHeight.intValue());
+		}
+		default void resize(double dWidth, double dHeight) throws LowerDimensionalOrderException {
+			resizeWidth(dWidth); resizeHeight(dHeight);
 		}
 	}
 }
