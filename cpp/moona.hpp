c64b3c49ca83c8499028ab3c5fc92e5e5363ae4f
= <pelleale261@gmail.com>
2023-04-04 21:10:16 +0200

    [813] removed include guard from moona.hpp header.

--- cpp/moona.hpp
+++ cpp/moona.hpp
@@ -1,14 +1,9 @@
 #pragma once
-
-#ifndef MOONA_MAIN
-    #define MOONA_MAIN
     
-    #include "interfaces/interfaces.hpp"
-    #include "strings/strings.hpp"
-    #include "exceptions/exceptions.hpp"
-    #include "collections/collections.hpp"
-    #include "hallway/hallway.hpp"
-    #include "base/base.hpp"
-    #include "util/utils.hpp"
-
-#endif
+#include "interfaces/interfaces.hpp"
+#include "strings/strings.hpp"
+#include "exceptions/exceptions.hpp"
+#include "collections/collections.hpp"
+#include "hallway/hallway.hpp"
+#include "base/base.hpp"
+#include "util/utils.hpp"
