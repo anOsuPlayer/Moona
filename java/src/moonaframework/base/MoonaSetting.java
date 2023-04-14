0efb6f7dd54cd25371bb0a9d2d087f376f0e49fa
= <pelleale261@gmail.com>
2023-04-14 23:12:56 +0200

    [848] now MoonaException(s) throw exceptions.
    Throwing exceptions gives off a better feedback compared to just doing nothing.

--- java/src/moonaframework/base/MoonaSetting.java
+++ java/src/moonaframework/base/MoonaSetting.java
@@ -4,23 +4,33 @@ import moonaframework.util.Setting;
 
 public final class MoonaSetting extends Setting {
 
-	public @Override void enable() {
-		if (!isFinal && !Moona.isOn()) { super.setValue(true); }
+	public @Override void enable() throws MoonaHandlingException {
+		if (!isFinal) {
+			if (Moona.isOn()) {
+				throw new MoonaHandlingException("MoonaSettings cannot be changed after Moona is started.");
+			}
+			super.setValue(true);
+		}
 	}
 	public void disable() {
-		if (!isFinal && !Moona.isOn()) { super.setValue(false); }
+		if (!isFinal) {
+			if (Moona.isOn()) {
+				throw new MoonaHandlingException("MoonaSettings cannot be changed after Moona is started.");
+			}
+			super.setValue(false);
+		}
 	}
 	
-	public MoonaSetting(boolean init, Setting...settings) {
+	protected MoonaSetting(boolean init, Setting...settings) {
 		super(init, settings);
 	}
-	public MoonaSetting(Setting...settings) {		
+	protected MoonaSetting(Setting...settings) {		
 		this(false, settings);
 	}
-	public MoonaSetting(boolean init) {
+	protected MoonaSetting(boolean init) {
 		this(init, new Setting[0]);
 	}
-	public MoonaSetting() {
+	protected MoonaSetting() {
 		this(false, new Setting[0]);
 	}
 }
