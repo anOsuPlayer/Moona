c3106a91698df6a8d714034aedc464301610c4ed
anOsuPlayer <pelleale261@gmail.com>
2023-04-26 13:38:37 +0200

    [875] added preferredCompiler field to Java Cpp class.
    It allows the user to select a preferred compiler (giving either the command alone or its full directory, as they'd write in a regular console).

--- java/src/moonaframework/hallway/Cpp.java
+++ java/src/moonaframework/hallway/Cpp.java
@@ -2,9 +2,21 @@ package moonaframework.hallway;
 
 import moonaframework.base.Moona;
 import moonaframework.base.MoonaHandlingException;
+import moonaframework.util.exception.NullArgumentException;
 
 public final class Cpp {
 
+	private static String preferredCompiler = "g++.exe";
+	
+	public static void setPreferredCompiler(String compiler) throws NullArgumentException {
+		if (!Moona.enableHallwayAccess.evaluate()) {
+			throw new HallwayAccessException();
+		}
+		if (compiler == null) {
+			throw new NullArgumentException("Unable to use a null Compiler.");
+		}
+	}
+	
 	public static native long version();
 	
 	public static String stringVersion() throws MoonaHandlingException {
