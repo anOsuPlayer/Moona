3d61022d4338480550d31e73da638b9571ff3981
anOsuPlayer <pelleale261@gmail.com>
2023-05-08 13:15:59 +0200

    [912] removed NativeLinker.
    It'll come back as soon as I figure out a more clever way of using it.

--- java/src/moonaframework/hallway/Address.java
+++ java/src/moonaframework/hallway/Address.java
@@ -2,7 +2,7 @@ package moonaframework.hallway;
 
 import moonaframework.util.functional.Satellite;
 
-public class MemoryAddress implements Satellite<java.lang.foreign.MemoryAddress> {
+public class Address implements Satellite<java.lang.foreign.MemoryAddress> {
 
 	protected final long address;
 	
@@ -13,18 +13,18 @@ public class MemoryAddress implements Satellite<java.lang.foreign.MemoryAddress>
 	public @Override java.lang.foreign.MemoryAddress translate() {
 		return java.lang.foreign.MemoryAddress.ofLong(this.address);
 	}
-	public @Override MemoryAddress build(java.lang.foreign.MemoryAddress ma) {
-		return new MemoryAddress(ma.toRawLongValue());
+	public @Override Address build(java.lang.foreign.MemoryAddress ma) {
+		return new Address(ma.toRawLongValue());
 	}
 	
 	public @Override String toString() {
 		return Long.toHexString(this.address);
 	}
 	public @Override boolean equals(Object o) {
-		return (o instanceof MemoryAddress ma) ? this.address == ma.address : false;
+		return (o instanceof Address ma) ? this.address == ma.address : false;
 	}
 	
-	public MemoryAddress(long address) {
+	public Address(long address) {
 		this.address = address;
 	}
 }
--- java/src/moonaframework/hallway/NativeArrayAllocation.java
+++
@@ -1,8 +0,0 @@
-package moonaframework.hallway;
-
-public final class NativeArrayAllocation extends NativeAllocation {
-
-	public NativeArrayAllocation(long address) {
-		super(address);
-	}
-}
