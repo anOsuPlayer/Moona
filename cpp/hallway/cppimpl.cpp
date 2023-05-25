7f473cd5a8b57eccf30c0680c89578856e3115ea
anOsuPlayer <pelleale261@gmail.com>
2023-05-25 10:04:54 +0200

    [956] removed test function from cppimpl.cpp file.

--- cpp/hallway/cppimpl.cpp
+++ cpp/hallway/cppimpl.cpp
@@ -4,13 +4,6 @@
 
 namespace moona {
 
-    JavaImpl jobject Java_test_Test_edit(DefaultArgs, jobject o) {
-        JavaMethod m = JavaClass("moonaframework.util.condition.Condition").getMethod("reverse", MethodSignature::VOID_METHOD);
-        m.callOn(o);
-
-        return o;
-    }
-
     JavaImpl jlong Java_moonaframework_hallway_Cpp_version(DefaultArgs) {
         return __cplusplus;
     }
