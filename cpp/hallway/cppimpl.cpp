a56d2e0219c692e75b7cb49829e93d1680aad1ee
= <pelleale261@gmail.com>
2023-06-09 18:44:33 +0200

    [973] removed useless implementation.

--- cpp/hallway/cppimpl.cpp
+++ cpp/hallway/cppimpl.cpp
@@ -3,11 +3,6 @@
 #include "../base/moonaclass.hpp"
 
 namespace moona {
-
-    JavaImpl void Java_test_Test_arr(StaticArgs, jobjectArray arr) {
-        JavaObjectArray a = arr;
-        std::cout << a.toString();
-    }
     
     JavaImpl jlong Java_moonaframework_hallway_Cpp_version(StaticArgs) {
         return __cplusplus;
